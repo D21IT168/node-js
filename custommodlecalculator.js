a6453e25cdb8196c8b5251fc5c487324ec52c822
D21IT168 <89838129+D21IT168@users.noreply.github.com>
2022-09-20 13:29:40 +0530

    Add files via upload

---
+++ custommodlecalculator.js
@@ -0,0 +1,40 @@
+exports.add = function (x, y) {
+    return x + y;
+};
+
+exports.sub = function (x, y) {
+    return x - y;
+};
+
+exports.mult = function (x, y) {
+    return x * y;
+};
+
+exports.div = function (x, y) {
+    return x / y;
+};
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var calculator = require('./custommodlecalculator');
+
+    var x = 50, y = 10;
+
+
+    res.write(" Addition of 50 and 10 is "
+                   + calculator.add(x, y));
+
+                   res.write("\n Subtraction of 50 and 10 is "
+                   + calculator.sub(x, y));
+
+                   res.write("\n Multiplication of 50 and 10 is "
+                   + calculator.mult(x, y));
+
+                   res.end("\n Division of 50 and 10 is "
+                   + calculator.div(x, y));
+                   res.send();
+                }).listen(3002);
